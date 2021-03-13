682d95c1af51a5640e871c9ebd29218fa1fbf83b
BillyBen <daniilkrivutsa141@gmail.com>
2021-03-13 09:38:36 +0300

    Changed Magican surname

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-alert("Greetings user! My name is Magican. Where are you from");
+alert("Greetings user! My name is Magican Red. Where are you from");
 document.write("Hello");
 document.write('<div>I am from Ukraine! Where are YOU from?<div>');
