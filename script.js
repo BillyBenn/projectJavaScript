8dec80121190b0c9d1166cab00ed1ebba3b29e13
BillyBen <daniilkrivutsa141@gmail.com>
2021-03-23 10:16:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 alert("Greetings user! My name is Magican Red. Where are you from");
+result = prompt(title, default);
 document.write("Hello");
 document.write('<div>I am from Ukraine! Where are YOU from?<div>');
